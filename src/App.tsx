ee66a06fcdde5b0886dd23b13c4ccf3222eca2ba
Corrine Wheeler <36771713+wheelercorrine@users.noreply.github.com>
2020-08-27 20:09:14 -0400

    A simple to do list application

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// Import dependencies
+import * as React from 'react'
+import { render } from 'react-dom'// Import components
+import TodoForm from './todoForm'
+import TodoList from './todoList'// Import interfaces
+import { TodoInterface } from './interfaces'// Import styles
+import './App.css'// TodoListApp component
+const App = () => {
+  const [todos, setTodos] = React.useState<TodoInterface[]>([])  // Creating new todo item
+  function handleTodoCreate(todo: TodoInterface) {
+    // Prepare new todos state
+    const newTodosState: TodoInterface[] = [...todos]    // Update new todos state
+    newTodosState.push(todo)    // Update todos state
+    setTodos(newTodosState)
+  }  // Update existing todo item
+  function handleTodoUpdate(event: React.ChangeEvent<HTMLInputElement>, id: string) {
+    // Prepare new todos state
+    const newTodosState: TodoInterface[] = [...todos]    // Find correct todo item to update
+    newTodosState.find((todo: TodoInterface) => todo.id === id)!.text = event.target.value    // Update todos state
+    setTodos(newTodosState)
+  }  // Remove existing todo item
+  function handleTodoRemove(id: string) {
+    // Prepare new todos state
+    const newTodosState: TodoInterface[] = todos.filter((todo: TodoInterface) => todo.id !== id)    // Update todos state
+    setTodos(newTodosState)
+  }  // Check existing todo item as completed
+  function handleTodoComplete(id: string) {
+    // Copy current todos state
+    const newTodosState: TodoInterface[] = [...todos]    // Find the correct todo item and update its 'isCompleted' key
+    newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted = !newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted    // Update todos state
+    setTodos(newTodosState)
+  }  // Check if todo item has title
+  function handleTodoBlur(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target.value.length === 0) {
+      event.target.classList.add('todo-input-error')
+    } else {
+      event.target.classList.remove('todo-input-error')
+    }
+  }  return (
+    <div className="todo-list-app">
+      <TodoForm
+        todos={todos}
+        handleTodoCreate={handleTodoCreate}
+      />      <TodoList
+        todos={todos}
+        handleTodoUpdate={handleTodoUpdate}
+        handleTodoRemove={handleTodoRemove}
+        handleTodoComplete={handleTodoComplete}
+        handleTodoBlur={handleTodoBlur}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/todoItem.tsx
@@ -0,0 +1,25 @@
+// Import dependencies
+import * as React from 'react'// Import interfaces
+import { TodoItemInterface } from './interfaces'// TodoItem component
+const TodoItem = (props: TodoItemInterface) => {
+  return (
+    <div className='todo-item'>
+      <div onClick={() => props.handleTodoComplete(props.todo.id)}>
+        {props.todo.isCompleted ? (
+          <span className="todo-item-checked">✔</span>
+        ) : (
+          <span className="todo-item-unchecked" />
+        )}
+      </div>      <div className="todo-item-input-wrapper">
+        <input
+          value={props.todo.text}
+          onBlur={props.handleTodoBlur}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => props.handleTodoUpdate(event, props.todo.id)}
+        />
+      </div>      <div className="item-remove" onClick={() => props.handleTodoRemove(props.todo.id)}>
+        ⨯
+      </div>
+    </div>
+  )
+}
+export default TodoItem
